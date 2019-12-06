63ac9b28945c579691aedf8e31c2e71381b08131
charles <charles.stolive@dayonmars.fr>
2019-12-06 16:24:52 +0100

    init crsm

---
+++ Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Waka\Crsm;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * crsm Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'crsm',
+            'description' => 'No description provided yet...',
+            'author'      => 'waka',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Waka\Crsm\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'waka.crsm.some_permission' => [
+                'tab' => 'crsm',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'crsm' => [
+                'label'       => 'crsm',
+                'url'         => Backend::url('waka/crsm/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['waka.crsm.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
