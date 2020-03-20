67487c0ee12a27e86a0dd99c063fa1122982fd2d
charles <charles.stolive@dayonmars.fr>
2020-03-20 23:35:48 +0100

    modif fonctions nouveau system fonction

--- functions/ClientFunctions.php
+++ functions/ClientFunctions.php
@@ -2,6 +2,7 @@
 
 namespace Waka\Crsm\Functions;
 
+use Waka\Crsm\Models\ProjectState;
 use Waka\Utils\Classes\BaseFunction;
 
 class ClientFunctions extends BaseFunction
@@ -21,12 +22,12 @@ class ClientFunctions extends BaseFunction
                 ],
             ],
             'ProjectByState' => [
-                'name' => "Tous les projet filtré sur un état",
+                'name' => "Tous les projet filtré sur un ou plusieurs états",
                 'attributes' => [
                     'project_state' => [
                         'label' => "Etat d'un projet",
-                        'type' => "dropdown",
-                        'options' => ['fuck', 'fiesta'],
+                        'type' => "taglist",
+                        'options' => ProjectState::lists('name', 'id'),
                     ],
                 ],
             ],
@@ -36,40 +37,45 @@ class ClientFunctions extends BaseFunction
                     'project_state' => [
                         'label' => "Etat d'un projet",
                         'type' => "dropdown",
-                        'options' => ['fuck', 'fiesta'],
+                        'options' => ProjectState::lists('name', 'id'),
                     ],
                     'start_date' => [
                         'label' => "Date de debut",
-                        'type' => "dropdown",
-                        'options' => ['fuck', 'fiesta'],
+                        'type' => "datepicker",
                     ],
                     'end_date' => [
                         'label' => "date de fin",
-                        'type' => "dropdown",
-                        'options' => ['fuck', 'fiesta'],
+                        'type' => "datepicker",
                     ],
                 ],
             ],
         ];
     }
 
-    public function ProjectActifsOrClosed($item)
+    public function ProjectActifsOrClosed($attributes)
     {
-        return $this->model->projects()->whereHas('project_state', function ($query) use ($item) {
-            $query->where('is_running', boolval($item['v1']));
+        $result = $this->model->projects()->whereHas('project_state', function ($query) use ($attributes) {
+            $query->where('is_running', boolval($attributes['active']));
         })->with('project_state')->get()->toArray();
+        trace_log($result);
+        return $result;
     }
 
-    public function projectByState($item)
+    public function projectByState($attributes)
     {
-        return $this->model->projects()->whereHas('project_state', function ($query) use ($item) {
-            $query->where('slug', $item['v1']);
+        $result = $this->model->projects()->whereHas('project_state', function ($query) use ($attributes) {
+            $query->whereIn('id', $attributes['project_state']);
         })->with('project_state')->get()->toArray();
+        trace_log($result);
+        return $result;
     }
 
-    public function projectByStateByDate($item)
+    public function projectByStateByDate($attributes)
     {
-        return $this->model->projects->width('project_state')->get()->toArray();
+        return $this->model->projects()->whereHas('project_state', function ($query) use ($attributes) {
+            $query->whereIn('id', $attributes['project_state']);
+        })->whereBetween('updated_at', [$attributes['start_date'], $attributes['end_date']])
+            ->with('project_state')->get()->toArray();
     }
 
 }
