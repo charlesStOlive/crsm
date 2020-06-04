38dadf2499d6fc06b3c0330715d8b8dfac73bc36
Charels <charles.stolive@gmail.com>
2020-06-04 13:01:38 +0200

    ajout de class dans config

--- config/config.php
+++ config/config.php
@@ -1,11 +1,15 @@
 <?php
 
 return [
-    'civ' => ['Mme/M.' => 'Mme/M.', 'Mme' => 'Mme', 'M.' => 'M.', 'Dr' => 'Dr', 'Pr' => 'Pr'],
-    'assets' => [
+    //obligatoire
+    'CloudClass' => 'Waka\Utils\Classes\Gd', // obligatoire pour cloud files dans model et futur traitement
+    'assets' => [ // obligatoire pour pdf
         'css' => [
             //si il  a du less penser à le mettre dans le registrer du plugin pour le combiner en css
             '/waka/crsm/assets/css/simple_grid/simple-table.css' => 'simple table',
         ],
     ],
+    //fin obligatoire
+    'civ' => ['Mme/M.' => 'Mme/M.', 'Mme' => 'Mme', 'M.' => 'M.', 'Dr' => 'Dr', 'Pr' => 'Pr'],
+    
 ];
